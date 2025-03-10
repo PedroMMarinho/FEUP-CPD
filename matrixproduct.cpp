6f6200c2089cb42e688092c0aa936b23701ef937
Xavier Martins <xavidosantos@gmail.com>
2025-03-10 11:47:16 +0000

    MultLine Parallelized InnerMost

--- matrixproduct.cpp
+++ matrixproduct.cpp
@@ -486,18 +486,92 @@ void OnMultLineParallelized(int m_ar, int m_br, double &timeTaken)
 	Time1 = clock();
 	start = omp_get_wtime();
 
-#pragma omp parallel
+	#pragma omp parallel 
 	{
+		#pragma omp for private(j, k)
+			for (int i = 0; i < m_ar; i++)
+			{
+				for (int j = 0; j < m_br; j++)
+				{
+					temp = pha[i * m_ar + j];
+					for (int k = 0; k < m_br; k++)
+					{
+						phc[i * m_ar + k] += temp * phb[j * m_ar + k];
+					}
+				}
+			}
+	}
 
-#pragma omp single
-		num_threads = omp_get_num_threads();
+	
 
-#pragma omp for private(j, k)
+	end = omp_get_wtime();
+	Time2 = clock();
+	// sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	// cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	/*~
+	cout << "Result matrix: " << endl;
+	for (i = 0; i < 1; i++)
+	{
+		for (j = 0; j < min(10, m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+	*/
+
+	double gflops = (2.0 * m_ar * m_ar * m_br) / ((end - start) * 1e6);
+	// printf("Performance: %.2f MFLOPS\n", gflops);
+	timeTaken = end - start;
+
+	free(pha);
+	free(phb);
+	free(phc);
+}
+
+
+void OnMultLineParallelizedInnerMost(int m_ar, int m_br, double &timeTaken)
+{
+
+	SYSTEMTIME Time1, Time2;
+	double start, end;
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	int num_threads = 1; // set default as 1
+	double *pha, *phb, *phc;
+
+	pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for (i = 0; i < m_ar; i++)
+		for (j = 0; j < m_ar; j++)
+			pha[i * m_ar + j] = (double)1.0;
+
+	for (i = 0; i < m_br; i++)
+		for (j = 0; j < m_br; j++)
+			phb[i * m_br + j] = (double)(i + 1);
+
+	for (i = 0; i < m_br; i++)
+		for (j = 0; j < m_br; j++)
+			phc[i * m_br + j] = (double)(0);
+
+	Time1 = clock();
+	start = omp_get_wtime();
+
+
+	#pragma omp parallel
+	{
 		for (int i = 0; i < m_ar; i++)
 		{
 			for (int j = 0; j < m_br; j++)
 			{
 				temp = pha[i * m_ar + j];
+				
+				#pragma omp for
+
 				for (int k = 0; k < m_br; k++)
 				{
 					phc[i * m_ar + k] += temp * phb[j * m_ar + k];
